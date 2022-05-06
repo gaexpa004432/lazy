cb3e97a80792390e794ed9dc7d2935d050f9b8d3
gaexpa004432 <gaexpa004432@naver.com>
2022-05-06 13:38:49 +0900

    cms add!

---
+++ CMS/src/main/java/admin/cms/main/AdminMainController.java
@@ -0,0 +1,16 @@
+package admin.cms.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminMainController {
+
+    @RequestMapping("/admin/main")
+    public String adminMain(Model model){
+
+        return "main/main";
+    };
+
+}
---
+++ CMS/src/test/java/admin/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package admin.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
