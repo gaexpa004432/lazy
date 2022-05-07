ab73a455744279b9be99fe1ecd361963f185c309
gaexpa004432 <gaexpa004432@naver.com>
2022-05-07 11:51:05 +0900

    login add!

--- CMS/src/main/java/admin/cms/main/MainController.java
+++ CMS/src/main/java/admin/cms/main/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class AdminMainController {
+public class MainController {
 
     @RequestMapping("/admin/main")
     public String adminMain(Model model){
